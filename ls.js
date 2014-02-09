7e7e4e6bab7e06d365eb5a1decaed37d19ad22b4
Devario Johnson <devariojohnson@devarios-mbp-2.home>
2014-02-09 15:20:00 -0500

    external js edit

--- ls.js
+++ ls.js
@@ -1,21 +1,19 @@
-<script type="text/javascript">
-	$(document).ready(function(){
-		var bg = localStorage.bgcolor;
-		var defaultColor = "white";
-		if(localStorage.getItem("bgcolor") === null){
-			$("#list").val("white");
-		}else{
-			$("#list").val(bg); 
-			$("body").css("background-color", bg);
-		}
-		$("#submitbtn").click(function(){
-			localStorage.bgcolor = $("#list").val();
-			$("body").css("background-color", localStorage.bgcolor);
-		});
-		$("#reset").click(function(){
-			localStorage.removeItem("bgcolor");
-			$("body").css("background-color", defaultColor);
-			$("#list").val(defaultColor);
-		});	
+$(document).ready(function(){
+	var bg = localStorage.bgcolor;
+	var defaultColor = "white";
+	if(localStorage.getItem("bgcolor") === null){
+		$("#list").val("white");
+	}else{
+		$("#list").val(bg); 
+		$("body").css("background-color", bg);
+	}
+	$("#submitbtn").click(function(){
+		localStorage.bgcolor = $("#list").val();
+		$("body").css("background-color", localStorage.bgcolor);
 	});
-</script>
+	$("#reset").click(function(){
+		localStorage.removeItem("bgcolor");
+		$("body").css("background-color", defaultColor);
+		$("#list").val(defaultColor);
+	});	
+});
